f31cc9af9d7b0d8e70ff2629820c7fe6ba391139
student <student@LAB-C-01>
2016-08-25 16:46:07 +0800

    third commit

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>Trying</title>
+        <title>Trying second time</title>
     </head>
     <body>
         <?php
